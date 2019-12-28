65e880260b4c1b5cbc752c8f2b6ca364bae58564
Jaeyoung Lee <jaeleeps@gmail.com>
2019-12-28 17:37:52 +0900

    pic

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="app"  class="back">
+    <v-app id="inspire">
+      <!-- <v-container> -->
+        <v-layout>
+          <v-row>
+            <v-flex xs0 md2></v-flex>
+            <v-flex xs12 md8>
+              <div class="about-layer">
+              </div>
+              <v-layout>
+                <v-flex xs12 md12>
+                  <v-card
+                    class="mx-auto"
+
+                    tile
+                  >
+                    <v-avatar>
+                      <img
+                        src="@/assets/profile_img_01.png"
+                        alt="John"
+                      >
+                    </v-avatar>
+                  </v-card>
+                  <img
+                    src="@/assets/profile_img_01.png"
+                    alt="John"
+                  >
+                </v-flex>
+              </v-layout>      
+
+
+            </v-flex>
+            <v-flex xs0 md2></v-flex>
+          </v-row>
+        </v-layout>
+    </v-app>
   </div>
 </template>
+
+<style scoped>
+.about-layer {
+
+}
+</style>
