65dbe897bf8b851dd6a23ca9467a0139916ebf00
Jaeyoung Lee <jaeleeps@gmail.com>
2019-12-29 09:22:48 +0900

    timeline init

--- src/App.vue
+++ src/App.vue
@@ -25,3 +25,7 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -18,7 +18,7 @@
                   </v-flex>
                   <v-flex xs12 md8>
                     <p class="contacts">
-                      <span class="font-weight-bold"> Jaeyoung Lee(이재영) </span>
+                      <span> JAEYOUNG <span class="font-weight-bold"> LEE </span></span>
                       <span class="font-weight-light">jaeleeps@gmail.com</span>
                       <span>8210.2309.4277</span>
                     </p>
@@ -28,7 +28,31 @@
               <v-row>
                 <v-flex xs12 md12>
                   <p class="academics">
-                    <span> Academics </span>
+                    <span class="subtitle"> Academics </span>
+                    <span>
+                      <v-timeline 
+                        align-top
+                        >
+                        <v-timeline-item
+                          v-for="item in academics_data"
+                          :key="item.index"
+                          color="grey lighten-2"
+                          small
+                        >
+                          <template v-slot:opposite>
+                            <span v-text="item.year"></span>
+                          </template>
+                          <v-card class="elevation-2">
+                            <v-card-title class="title" v-text="item.title" />
+                            <v-card-text
+                             style="padding-top: 0px; padding-bottom: 8px;"
+                             v-for="text in item.info"
+                             :key = text.length 
+                             v-text="text" />
+                          </v-card>
+                        </v-timeline-item>
+                      </v-timeline>
+                    </span>
                   </p>
                 </v-flex>  
               </v-row>     
@@ -40,17 +64,41 @@
   </div>
 </template>
 
-<style scoped>
-.about-layer {
-
+<script>
+export default {
+  data: () => ({
+    academics_data : [
+      {
+        index: 0,
+        year: "2014~2017",
+        title: "Hankuk Academy of Foreign Studides" ,
+        info: ["International Div. Class of 2017", " Major in Chinese Language", "GPA: 3.88/4.0" ],
+      },
+      {
+        index: 0,
+        year: "2014~2017",
+        title: "Hankuk Academy of Foreign Studides" ,
+        info: ["International Div. Class of 2017", " Major in Chinese Language", "GPA: 3.88/4.0" ],
+      }
+    ]
+  })
 }
+</script>
 
-.contacts span 
+<style scoped>
+.contacts > span 
 {
   /* font-family: Druk Wide Cy TT Medium; */
   font-family: Optima;
   /* font-size: 2rem; */
   display: block;
+}
+
+.subtitle {
+  font-size: 2rem;
+  /* font-weight: bold; */
+  text-transform: uppercase;
+  letter-spacing: 5px;
 }
 
 .academics {
