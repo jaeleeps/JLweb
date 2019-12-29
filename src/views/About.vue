a8a430f24e01aafa8a5e39ede056a475ac6e991a
Jaeyoung Lee <jaeleeps@gmail.com>
2019-12-29 14:23:39 +0900

    skills init

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,7 +8,7 @@
             <v-flex xs12 md6>
               <v-row>
                 <v-layout>
-                  <v-flex xs12 md4>
+                  <v-flex xs12 md6>
                     <v-avatar tile size="200">
                       <img
                         src="@/assets/profile_img_01.png"
@@ -16,9 +16,12 @@
                       >
                     </v-avatar>
                   </v-flex>
-                  <v-flex xs12 md8>
+                  <v-flex xs12 md6>
                     <p class="contacts">
-                      <span> JAEYOUNG <span class="font-weight-bold"> LEE </span></span>
+                      <span 
+                        style="letter-spacing: 4px; font-size: 2rem;" > 
+                        JAEYOUNG <span style="font-weight: 400;"> LEE </span>
+                      </span>
                       <span class="font-weight-light">jaeleeps@gmail.com</span>
                       <span>8210.2309.4277</span>
                     </p>
@@ -27,12 +30,10 @@
               </v-row>
               <v-row>
                 <v-flex xs12 md12>
-                  <p class="academics">
+                  <p class="about-para">
                     <span class="subtitle"> Academics </span>
                     <span>
-                      <v-timeline 
-                        align-top
-                        >
+                      <v-timeline>
                         <v-timeline-item
                           v-for="item in academics_data"
                           :key="item.index"
@@ -55,7 +56,35 @@
                     </span>
                   </p>
                 </v-flex>  
-              </v-row>     
+              </v-row>
+              <v-row>
+                <v-flex xs12 md12>
+                  <p class="about-para">
+                    <span class="subtitle"> Skills </span>
+                    <span class="subsubtitle">Programming</span>
+                    <span>
+                      <v-card
+                        max-width="400"
+                        class="mx-auto"
+                      >
+                        <v-card-text>
+                          <v-chip-group
+                            multiple
+                            column
+                            active-class="primary--text"
+                          >
+                            <v-chip v-for="tag in tags" :key="tag">
+                              <v-icon left color="red">mdi-account</v-icon>
+                              {{ tag.title }}
+                            </v-chip>
+                          </v-chip-group>
+                        </v-card-text>
+                      </v-card>
+                    </span>
+                    <span class="subsubtitle">Language</span>
+                  </p>
+                </v-flex>
+              </v-row>
             </v-flex>
             <v-flex xs0 md3></v-flex>
           </v-row>
@@ -76,30 +105,54 @@ export default {
       },
       {
         index: 0,
-        year: "2014~2017",
-        title: "Hankuk Academy of Foreign Studides" ,
-        info: ["International Div. Class of 2017", " Major in Chinese Language", "GPA: 3.88/4.0" ],
+        year: "2017~",
+        title: "Georgia Institue of Technology" ,
+        info: ["Bachelor of Science in Computer Science", "Minor in Industrial Design", "Major GPA: 4.0/4.0", "Overall GPA: 3.86/4.0" ],
+      },
+    ],
+    tags: [
+      {
+        title: "Python",
       }
-    ]
+    ],
   })
 }
 </script>
 
 <style scoped>
+.about-para {
+  margin-top: 3rem;
+}
+
+.about-para > span {
+  display: block;
+}
+
+.about-para > .subtitle {
+  font-size: 2rem;
+  font-weight: 400;
+  text-transform: uppercase;
+  letter-spacing: 5px;
+}
+
+.about-para > .subsubtitle {
+  font-size: 1.3rem;
+  font-weight: 300;
+  text-transform: uppercase;
+  letter-spacing: 4px;
+}
+
+
 .contacts > span 
 {
   /* font-family: Druk Wide Cy TT Medium; */
-  font-family: Optima;
+  /* font-family: Optima; */
+  font-weight: 200;
   /* font-size: 2rem; */
   display: block;
 }
 
-.subtitle {
-  font-size: 2rem;
-  /* font-weight: bold; */
-  text-transform: uppercase;
-  letter-spacing: 5px;
-}
+
 
 .academics {
 
