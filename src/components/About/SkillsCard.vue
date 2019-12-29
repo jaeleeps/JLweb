009ec9f273f135cdee7cb6d8f4da12707a29c648
Jaeyoung Lee <jaeleeps@gmail.com>
2019-12-29 15:29:23 +0900

    skills card

---
+++ src/components/About/SkillsCard.vue
@@ -0,0 +1,95 @@
+<template>
+  <!-- <v-card max-width="400" class="mx-auto">
+    <v-card-text>
+      <v-chip-group multiple column>
+        <v-tooltip bottom v-for="tag in tags" :key="tag">
+          <template v-slot:activator="{ on }">
+            <v-chip>
+              <v-icon left color="red">mdi-account</v-icon>
+              {{ tag.title }}
+            </v-chip>
+          </template>
+          <span>Tooltip</span>
+        </v-tooltip>
+      </v-chip-group>
+    </v-card-text>
+  </v-card> -->
+  <v-card max-width="400" class="mx-auto">
+    <v-card-text>
+      <v-menu
+        v-for="(tag, i) in tags" 
+        :key="i"
+        bottom
+        right
+        transition="scale-transition"
+        origin="top left"
+      >
+        <template v-slot:activator="{ on }">
+          <v-chip
+            pill
+            v-on="on"
+          >
+            <v-icon left :color="tag.color">{{tag.icon}}</v-icon>
+            {{tag.title}}
+          </v-chip>
+        </template>
+        <v-card width="300">
+          <v-list dark>
+            <v-list-item>
+              <v-list-item-avatar>
+                <v-icon left>{{tag.icon}}</v-icon>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>{{tag.title}}</v-list-item-title>
+                <v-list-item-subtitle>{{tag.rating}}</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn
+                  icon
+                  @click="menu = false"
+                >
+                  <v-icon>mdi-close-circle</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+          <!-- <v-list>
+            <v-list-item @click="() => {}">
+              <v-list-item-action>
+                <v-icon>mdi-briefcase</v-icon>
+              </v-list-item-action>
+              <v-list-item-subtitle>john@gmail.com</v-list-item-subtitle>
+            </v-list-item>
+          </v-list> -->
+        </v-card>
+      </v-menu>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    tags: [
+      {
+        title: "Javascript",
+        color: "red",
+        icon: "mdi-account",
+        rating: 5,
+      },
+      {
+        title: "Java",
+        color: "primary",
+        icon: "mdi-account",
+        rating: 5,
+      },
+        {
+        title: "Python",
+        color: "primary",
+        icon: "mdi-account",
+        rating: 5,
+      }
+    ]
+  })
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,122 +1,113 @@
 <template>
-  <div id="app"  class="back">
+  <div id="app" class="back">
     <v-app id="inspire">
       <!-- <v-container> -->
-        <v-layout>
-          <v-row>
-            <v-flex xs0 md3></v-flex>
-            <v-flex xs12 md6>
-              <v-row>
-                <v-layout>
-                  <v-flex xs12 md6>
-                    <v-avatar tile size="200">
-                      <img
-                        src="@/assets/profile_img_01.png"
-                        alt="John"
-                      >
-                    </v-avatar>
-                  </v-flex>
-                  <v-flex xs12 md6>
-                    <p class="contacts">
-                      <span 
-                        style="letter-spacing: 4px; font-size: 2rem;" > 
-                        JAEYOUNG <span style="font-weight: 400;"> LEE </span>
-                      </span>
-                      <span class="font-weight-light">jaeleeps@gmail.com</span>
-                      <span>8210.2309.4277</span>
-                    </p>
-                  </v-flex>
-                </v-layout> 
-              </v-row>
-              <v-row>
-                <v-flex xs12 md12>
-                  <p class="about-para">
-                    <span class="subtitle"> Academics </span>
-                    <span>
-                      <v-timeline>
-                        <v-timeline-item
-                          v-for="item in academics_data"
-                          :key="item.index"
-                          color="grey lighten-2"
-                          small
-                        >
-                          <template v-slot:opposite>
-                            <span v-text="item.year"></span>
-                          </template>
-                          <v-card class="elevation-2">
-                            <v-card-title class="title" v-text="item.title" />
-                            <v-card-text
-                             style="padding-top: 0px; padding-bottom: 8px;"
-                             v-for="text in item.info"
-                             :key = text.length 
-                             v-text="text" />
-                          </v-card>
-                        </v-timeline-item>
-                      </v-timeline>
+      <v-layout>
+        <v-row>
+          <v-flex xs0 md3></v-flex>
+          <v-flex xs12 md6>
+            <v-row>
+              <v-layout>
+                <v-flex xs12 md6>
+                  <v-avatar tile size="200">
+                    <img src="@/assets/profile_img_01.png" alt="John" />
+                  </v-avatar>
+                </v-flex>
+                <v-flex xs12 md6>
+                  <p class="contacts">
+                    <span style="letter-spacing: 4px; font-size: 2rem;">
+                      JAEYOUNG
+                      <span style="font-weight: 400;">LEE</span>
                     </span>
-                  </p>
-                </v-flex>  
-              </v-row>
-              <v-row>
-                <v-flex xs12 md12>
-                  <p class="about-para">
-                    <span class="subtitle"> Skills </span>
-                    <span class="subsubtitle">Programming</span>
-                    <span>
-                      <v-card
-                        max-width="400"
-                        class="mx-auto"
-                      >
-                        <v-card-text>
-                          <v-chip-group
-                            multiple
-                            column
-                            active-class="primary--text"
-                          >
-                            <v-chip v-for="tag in tags" :key="tag">
-                              <v-icon left color="red">mdi-account</v-icon>
-                              {{ tag.title }}
-                            </v-chip>
-                          </v-chip-group>
-                        </v-card-text>
-                      </v-card>
-                    </span>
-                    <span class="subsubtitle">Language</span>
+                    <span class="font-weight-light">jaeleeps@gmail.com</span>
+                    <span>8210.2309.4277</span>
                   </p>
                 </v-flex>
-              </v-row>
-            </v-flex>
-            <v-flex xs0 md3></v-flex>
-          </v-row>
-        </v-layout>
+              </v-layout>
+            </v-row>
+            <v-row>
+              <v-flex xs12 md12>
+                <p class="about-para">
+                  <span class="subtitle">Academics</span>
+                  <span>
+                    <v-timeline>
+                      <v-timeline-item
+                        v-for="item in academics_data"
+                        :key="item.index"
+                        color="grey lighten-2"
+                        small
+                      >
+                        <template v-slot:opposite>
+                          <span v-text="item.year"></span>
+                        </template>
+                        <v-card class="elevation-2">
+                          <v-card-title class="title" v-text="item.title" />
+                          <v-card-text
+                            style="padding-top: 0px; padding-bottom: 8px;"
+                            v-for="text in item.info"
+                            :key="text.length"
+                            v-text="text"
+                          />
+                        </v-card>
+                      </v-timeline-item>
+                    </v-timeline>
+                  </span>
+                </p>
+              </v-flex>
+            </v-row>
+            <v-row>
+              <v-flex xs12 md12>
+                <p class="about-para">
+                  <span class="subtitle">Skills</span>
+                  <span class="subsubtitle">Programming</span>
+                  <span>
+                    <SkillsCard />
+                  </span>
+                  <span class="subsubtitle">Language</span>
+                </p>
+              </v-flex>
+            </v-row>
+          </v-flex>
+          <v-flex xs0 md3></v-flex>
+        </v-row>
+      </v-layout>
     </v-app>
   </div>
 </template>
 
 <script>
+import SkillsCard from "@/components/About/SkillsCard";
+
 export default {
+  components: {
+    SkillsCard
+  },
   data: () => ({
-    academics_data : [
+    academics_data: [
       {
         index: 0,
         year: "2014~2017",
-        title: "Hankuk Academy of Foreign Studides" ,
-        info: ["International Div. Class of 2017", " Major in Chinese Language", "GPA: 3.88/4.0" ],
+        title: "Hankuk Academy of Foreign Studides",
+        info: [
+          "International Div. Class of 2017",
+          " Major in Chinese Language",
+          "GPA: 3.88/4.0"
+        ]
       },
       {
         index: 0,
         year: "2017~",
-        title: "Georgia Institue of Technology" ,
-        info: ["Bachelor of Science in Computer Science", "Minor in Industrial Design", "Major GPA: 4.0/4.0", "Overall GPA: 3.86/4.0" ],
-      },
-    ],
-    tags: [
-      {
-        title: "Python",
+        title: "Georgia Institue of Technology",
+        info: [
+          "Bachelor of Science in Computer Science",
+          "Minor in Industrial Design",
+          "Major GPA: 4.0/4.0",
+          "Overall GPA: 3.86/4.0"
+        ]
       }
-    ],
+    ]
   })
-}
+};
 </script>
 
 <style scoped>
@@ -142,9 +133,7 @@ export default {
   letter-spacing: 4px;
 }
 
-
-.contacts > span 
-{
+.contacts > span {
   /* font-family: Druk Wide Cy TT Medium; */
   /* font-family: Optima; */
   font-weight: 200;
@@ -152,9 +141,6 @@ export default {
   display: block;
 }
 
-
-
 .academics {
-
 }
 </style>
