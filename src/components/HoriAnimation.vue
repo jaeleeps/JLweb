e16192e7cef7bb56a179b63d5690d5b651a9f367
Jaeyoung Lee <jaeleeps@gmail.com>
2020-01-24 20:55:24 +0900

    design page under construction

---
+++ src/components/HoriAnimation.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <p>
+      <v-icon>mdi-home</v-icon>
+  </p>
+</div>
+</template>
+
+<style scoped>
+div {
+    width: 100vw;
+    overflow: hidden;
+}
+
+p {
+  animation-duration: 2s;
+  animation-name: slidein;
+  animation-iteration-count: infinite;
+  animation-direction: reverse;
+}
+
+@keyframes slidein {
+  from {
+    margin-left: 90%;
+    width: 300%;
+  }
+
+  to {
+    margin-left: 0%;
+    width: 100%;
+  }
+}
+</style>
--- src/views/Design.vue
+++ src/views/Design.vue
@@ -1,28 +1,24 @@
 <template>
-  <div class="back">
+  <div class="back pa-10">
     <v-content>
       <v-container fill-height>
         <v-layout justify-center align-center pb-10>
           <v-flex shrink xs11 md10>
             <v-row>
-              <span class="cat_title">About</span>
-            </v-row>
-
-            <v-row>
-              <span class="cat_subtitle">About & Contacts</span>
-            </v-row>
-
-            <v-row>
-
+              <span class="cat_title">Design</span>
             </v-row>
 
             <v-row class="mb-2 cat_subsubtitle" style="text-align: center;" justify="center">
-              <span style="font-size: 0.75rem;"> click to know more about me </span>
-            </v-row>
-            <v-row class="mb-2" style="text-align: center;" justify="center">
-              <DownArrow />
+              <!-- <HoriAnimation /> -->
+              <v-progress-circular :size="50" color="#d2b49b" indeterminate></v-progress-circular>
             </v-row>
 
+            <v-row class="ma-5 cat_subsubtitle" style="text-align: center;" justify="center">
+              <span style="font-size: 0.75rem;">Design Page is under construction</span>
+            </v-row>
+            <v-row class="mb-2" style="text-align: center;" justify="center">
+              <!-- <DownArrow /> -->
+            </v-row>
           </v-flex>
         </v-layout>
       </v-container>
@@ -33,19 +29,49 @@
 <script>
 import MainCarousel from "@/components/MainCarousel";
 import ProjectCard from "@/components/ProjectCard";
+import HoriAnimation from "@/components/HoriAnimation";
 
 export default {
   components: {
     MainCarousel,
-    ProjectCard
+    ProjectCard,
+    HoriAnimation
   }
 };
 </script>
 
 <style scoped>
-.back{
+.cat_title {
+  font-size: 2rem;
+  font-weight: 400;
+  text-transform: uppercase;
+  letter-spacing: 5px;
+  margin-bottom: 1rem;
+  color: #212121;
+}
+
+.cat_subtitle {
+  font-size: 1.5rem;
+  font-weight: 400;
+  text-transform: uppercase;
+  letter-spacing: 5px;
+  margin-bottom: 0px;
+}
+
+.cat_subsubtitle {
+  font-size: 1.3rem;
+  /* @media $display-breakpoints.xs-only {
+    font-size: 5px;
+  } */
+  font-weight: 300;
+  text-transform: uppercase;
+  letter-spacing: 4px;
+  /* margin-left: 20rem; */
+}
+
+.back {
   /* width: 100vw;
   min-height: 100vh; */
-  background-color: #EFEBE9;
+  background-color: #efebe9;
 }
 </style>
