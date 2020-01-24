0130e8c8104d8d6f99e4bf056d4c0de0a0e502e0
Jaeyoung Lee <jaeleeps@gmail.com>
2020-01-24 14:40:23 +0900

    projects align center

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="back">
-    <v-container fluid>
+    <v-container fluid justify-center align-center>
       <v-layout justify-center align-center>
-        <v-flex shrink xs11 md8>
-          <span class="title">Projects</span>
-          <ProjectCard />
-        </v-flex>
+        <v-row align="center" justify="center">
+          <v-flex shrink xs11 md8>
+            <span class="title">Projects</span>
+            <ProjectCard />
+          </v-flex>
+        </v-row>
       </v-layout>
     </v-container>
   </div>
@@ -26,8 +28,15 @@ export default {
 <style scoped>
 .back {
   width: 100vw;
+  height: 100%;
   min-height: 100vh;
   background-color: #d2b29b;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
 }
 
 .title {
