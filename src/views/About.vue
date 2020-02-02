76fbe0d9a611a152cdd3f4dd98d27cafa7570425
Jaeyoung Lee <jaeleeps@gmail.com>
2020-02-02 19:24:02 +0900

    ADD: babel for IE, UPDATE: about ETC info

--- src/views/About.vue
+++ src/views/About.vue
@@ -163,15 +163,81 @@
 
             <transition name="fade">
               <v-row v-show="show_etc">
-                <v-layout flex-wrap>
-                  <span class="cat_subtitle mb-5">Etc.</span>
-                  <v-flex xs12 md12>
-                    <span class="cat_subsubtitle ma-5">Honors & Awards</span>
+                <v-layout flex-wrap justify-space-between>
+                  <v-flex xs12 md12 mb-3>
+                    <span class="cat_subtitle mb-5">Etc.</span>
                   </v-flex>
-                  <v-flex xs12 md12>
-                    <span class="cat_subsubtitle ma-5">Certificates</span>
+                  <v-flex xs12 md6 pa-4>
+                    <span class="cat_subsubtitle ma-3">Honors & Awards</span>
+                    <v-card
+                      v-for="(item, i) in etc_data.honors_data"
+                      :key="i"
+                      class="mx-auto ma-10"
+                      outlined
+                    >
+                      <v-list-item three-line>
+                        <v-list-item-content>
+                          <div class="overline mb-1">{{item.years}}</div>
+                          <v-list-item-avatar tile size="80" color="transparent">
+                            <v-img :src="item.img_link" contain />
+                          </v-list-item-avatar>
+                          <div
+                            class="mb-2"
+                            style="font-weight: 500; font-size: 1.2rem;"
+                          >{{item.title}}</div>
+                          <v-list-item-text class="mb-2">{{item.subtitle}}</v-list-item-text>
+                          <v-expand-transition>
+                            <div v-show="item.show">
+                              <!-- <v-divider></v-divider> -->
+                              <v-list-item-subtitle
+                                class="ma-2"
+                                v-for="(j, txt) in item.contents"
+                                :key="j"
+                              >{{j}}</v-list-item-subtitle>
+                              <!-- ㆍ  -->
+                            </div>
+                          </v-expand-transition>
+                        </v-list-item-content>
+                      </v-list-item>
+                      <v-card-actions>
+                        <v-spacer />
+                        <v-btn class="mt-n5" icon @click="item.show = !item.show">
+                          <v-icon>{{ item.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                        </v-btn>
+                        <v-spacer />
+                      </v-card-actions>
+                    </v-card>
                   </v-flex>
 
+                  <v-flex xs12 md6 pa-4>
+                    <span class="cat_subsubtitle ma-3">Certificates</span>
+                    <v-card
+                      v-for="(item, i) in etc_data.cert_data"
+                      :key="i"
+                      class="mx-auto ma-10"
+                      outlined
+                    >
+                      <v-list-item three-line>
+                        <v-list-item-content>
+                          <div class="overline mb-1">{{item.years}}</div>
+                          <v-list-item-avatar tile size="80" color="transparent">
+                            <v-img :src="item.img_link" contain />
+                          </v-list-item-avatar>
+                          <div
+                            class="mb-2"
+                            style="font-weight: 500; font-size: 1.2rem;"
+                          >{{item.title}}</div>
+                          <!-- <v-list-item-title class="headline mb-1">{{item.title}}</v-list-item-title> -->
+                          <v-list-item-text class="mb-2">{{item.subtitle}}</v-list-item-text>
+                          <v-list-item-subtitle
+                            class="mb-1"
+                            v-for="(j, txt) in item.contents"
+                            :key="j"
+                          >- {{j}}</v-list-item-subtitle>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-card>
+                  </v-flex>
 
                   <v-flex xs12 md12>
                     <v-sheet
@@ -236,6 +302,110 @@ export default {
     show_exp: false,
     show_skills: false,
     show_etc: false,
+    etc_data: {
+      honors_data: [
+        {
+          years: "2019",
+          title:
+            "2019 ROK Ministry of National Defense S/W Intelligence Competition",
+          subtitle: "Finalist",
+          img_link:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Emblem_of_the_Ministry_of_National_Defense_%28South_Korea%29_%28Korean%29.svg/1280px-Emblem_of_the_Ministry_of_National_Defense_%28South_Korea%29_%28Korean%29.svg.png",
+          contents: [
+            "Auspices: ROK Ministry of National Defense, ROK Ministry of Science and ICT, OSAM(Open Source Academy for Military)",
+            "Went through coding test, web knowledge test, and application evaluation and was selected as one of WEB division finalists.",
+            "Received a week of WEB group instruction, mainly about front-end."
+          ],
+          show: false
+        },
+        {
+          years: "2019",
+          title: "2019 ROK Ministry of National Defense Open Data Idea Contest",
+          subtitle: "Finalist",
+          img_link: "https://www.data.go.kr/assets/imgs/opengraph.png",
+          contents: [
+            "Auspices: ROK Ministry of National Defense, ROK Public Data Portal",
+            "Established development plan for an Android application providing useful infotmation for Korean soldier and soldier-to-be",
+            "Gave an APP UI demonstration and presentation at National Defense Ministry of Korea."
+          ],
+          show: false
+        },
+        {
+          years: "2016",
+          title: "SAGE Korea 2016",
+          subtitle: "Grand Prize(1st place) & Best Innovative Award",
+          img_link:
+            "https://scontent-ssn1-1.xx.fbcdn.net/v/t31.0-8/13305080_1197571036940438_225372631679600103_o.jpg?_nc_cat=110&_nc_ohc=7UfLMYJGFO4AX89u1T5&_nc_ht=scontent-ssn1-1.xx&oh=dbe07bbda7287b1f59e9eb730fec3569&oe=5E8FCCBA",
+          contents: [
+            "Auspices: SAGE Korea",
+            "Managed Piece, social enterprise business selling art puzzles illustrated by savant syndrome patients and distributing its revenue to those patients.",
+            "Selected as a national Team and advanced to SAGE World Cup 2016."
+          ],
+          show: false
+        },
+        {
+          years: "2016",
+          title: "GEC Korea Enterprise Challenge 2016 Winter",
+          subtitle: "Gold Prize(1st place)",
+          img_link: "https://www.gayoungpark.com/images/geckorea.png",
+          contents: [
+            "Auspices: GEC(Global Entrepreneur Challenge) Korea",
+            "Established a business strategy for ONGO, cultural asset restoration and replica production service using 3D printing technology."
+          ],
+          show: false
+        },
+        {
+          years: "2016",
+          title: "Wharton Korea Business Black Box",
+          subtitle: "Ranked top 5",
+          img_link:
+            "https://scontent-ssn1-1.xx.fbcdn.net/v/t1.0-9/31100376_1833188946975674_7090591878680150016_n.png?_nc_cat=104&_nc_ohc=_Z3xOGCfsWMAX_O5jaF&_nc_ht=scontent-ssn1-1.xx&oh=c1b7bbadac11fc3cb2e66a6ab7fd1776&oe=5EC75A34",
+          contents: [
+            "Auspices: ROK Ministry of National Defense, ROK Public Data Portal, Baemin(배달의민족)",
+            "Set a business plan for baby food regular delivery service, Baemin for Mom, under the competition’s theme ‘F&B market entry starategem for for Baemin(배달의민족)’.",
+            "Designed a logo and a rough version of application interface for the service and set a marketing plan"
+          ],
+          show: false
+        }
+        // {
+        //   years: "2016",
+        //   title: "The 7th Korea Youth Creative Design Competition",
+        //   subtitle: "Exellence Award(2nd place)",
+        //   img_link: "http://kbatv.org/_images/sub/symbol.jpg",
+        //   contents: [
+        //     "Auspices: KBAS(Korea Broadcasting Art School)",
+        //     "Designed a poster to raise public awareness on cyber bullying on SNS and set an advertising plan for it."
+        //   ],
+        //   show: false
+        // }
+      ],
+      cert_data: [
+        {
+          years: "2019",
+          title: "CIP(Craftsman Information Processing) Certificate",
+          subtitle: "Certificate",
+          img_link: "https://msit.go.kr/cms/images/www/sub/sub6_1_3_img4.png",
+          contents: [],
+          show: false
+        },
+        {
+          years: "2019",
+          title: "HSK(汉语水平考试) level 4",
+          subtitle: "Chinese Proficiency Test",
+          img_link: "https://post-phinf.pstatic.net/MjAxODEyMDdfOTEg/MDAxNTQ0MTQ4MTA2NjY1.gRNbns047ZSx79a-JMUHYWXkWMlt4IKOSkmF8xYKwB8g.OTwnBTCWMk52WYneMFR6TpISwFawmdAEsmvogAxnQuIg.JPEG/080720151701300.jpg?type=w1200",
+          contents: [],
+          show: false
+        },
+        {
+          years: "2016",
+          title: "ETS TOEFL(Test Of English as a Foreign Language)",
+          subtitle: "Score: 108",
+          img_link: "https://img.emg-services.net/HtmlPages/HtmlPage15627/ets-toefl-4cd.png",
+          contents: [],
+          show: false
+        }
+      ]
+    },
     contacts_btn: [
       {
         contact_icon: "mdi-linkedin",
