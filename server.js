9d8d024a3c2dfa0263c82bbb24783eaf15b19ddd
이재영(도마) <jaeleeps@jaranda.kr>
2020-06-23 23:52:19 +0900

    temporary redirect strategy for miracle scope

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const path = require('path')
 const app = express()
 
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use('/miracle-scope-solar', function(req, res) {
+	res.status(301).redirect('//198.13.51.149/solar/')
+});
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
